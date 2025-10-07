6bbfb5b422eee3fa4c9b10f0b79cbe3e89c8754e
Senra04 <senra04@gmail.com>
2025-10-07 19:59:51 +0200

    OK

--- CODIGOS_PRO/DAM/T1/ProyectoInicial/src/Main.java
+++ CODIGOS_PRO/DAM/T1/ProyectoInicial/src/Main.java
@@ -1,11 +1,45 @@
+// para bajar o subir las lineas de codigo ALT+Shift Flecha arriba y abajo
 public class Main {
-
     public static void main(String[] args) {
-        System.out.print("Hola, este es mi primer programa en Java.\nEsta es la segunda linea\n");
-        System.out.print("Tercera linea");
-        System.out.print("Cuarta linea");
+
+
+        String nombreCompleto = "Diego Senra"; //los String son palabras y van entrecomilladas dobles
+        int edad = 34;
+        edad = edad +1; //esto modifica al int edad
+        float altura = 1.71f; //se pone la f final para que no lo interprete como double
+        double peso = 70.50;
+        char letraCarnet = 'M';
+        char letraCarnetnumero = 470; // caracter raro---> uso para cifrado y descifrado
+        boolean experiencia = true; // utilizada para hacer comprobaciones condicionales
+        Object cosa = true; // engloba a todas las variables anteriores
+
+
+
+
+
+        System.out.println("Hola, este es mi primer programa en Java.");
+        System.out.println("Mi nombre es "+nombreCompleto);
+        System.out.println("Mi edad es "+edad+" años");
+        System.out.println("Mi altura es "+altura+" cm");
+        System.out.println("Mi peso es "+peso+" kg");
+        System.out.println("Mi letra del carnet es "+ letraCarnet);
+        System.out.printf("Tengo experiencia "+experiencia);
+
+
+
+
+
+          /* entre estos simbolos se ponen comentarios y el comentario al estar entre simbolos se puede cerrar
+          */
+
+        /**
+         *comentario
+         */
+
+
     }
 
+    //TODO Pendien revisar proyecto
 }
 
 
