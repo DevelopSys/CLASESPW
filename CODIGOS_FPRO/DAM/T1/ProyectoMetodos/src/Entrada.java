e92d9fa2fb6c7f0ba059fd1636d44035aa2fadb6
Senra04 <senra04@gmail.com>
2025-10-10 16:52:25 +0200

    Cambios ok
    Cambios ok

---
+++ CODIGOS_FPRO/DAM/T1/ProyectoMetodos/src/Entrada.java
@@ -0,0 +1,31 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto metodos");
+        System.out.println("Vamos a realizar llamadas a metodos");
+        System.out.println("Vamos a practicar el llamado de metodos");
+        /*saludar("Borja");
+        saludar("Laura");
+        despedir();
+         */
+
+    }
+
+public static void saludar(String nombre) {
+            System.out.println("Hola "+nombre);
+            System.out.println("Encantado de saludarte, vamos a practicar");
+            System.out.println("Vamos a practicar el llamado de metodos");
+            System.out.println("Animo, no decaigas en tu tarea");
+        }
+
+public static void despedir(){
+    System.out.println("A casa majo");
+}
+
+
+
+
+
+
+
+}
---
+++ CODIGOS_FPRO/DAM/T1/ProyectoMetodos/src/Operaciones.java
@@ -0,0 +1,23 @@
+public class Operaciones {
+
+    //main??
+    //variables
+    //metodos -> funcionalidades que quieres poder ejecutar
+    public void sumar(int op1, int op2){
+int resultado
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
