8860f777bdf8acbdbbb6b7f2cb967f65417c3d06
Develop and System <developandsystem@gmail.com>
2026-02-17 19:57:43 +0100

    update

---
+++ CODIGOS_PRO/DAM/T7/Flujos/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import controller.GestorFicheros;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GestorFicheros gestor = new GestorFicheros();
+        // gestor.crearFichero("src/main/java/recursos/escritura.txt");
+        // gestor.crearCarpeta("src/main/java/recursos/cosa/ejemplo");
+        // gestor.lectorFichero("src/main/java/recursos/lectura.txt");
+        // gestor.lectorLineas("src/main/java/recursos/lectura.txt");
+        // gestor.escribirCaracteres("src/main/java/recursos/escritura.txt");
+        //gestor.escribirLineas("src/main/java/recursos/escritura.txt");
+        // gestor.codificarMensaje("src/main/java/recursos/codificado.txt");
+        gestor.descifrarMenasje("src/main/java/recursos/codificado.txt");
+    }
+}
