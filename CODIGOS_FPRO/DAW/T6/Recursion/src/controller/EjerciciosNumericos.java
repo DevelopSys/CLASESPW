78831b40a4a69e2348090a540d748e27e4c85ed2
Develop and System <developandsystem@gmail.com>
2026-03-20 16:00:13 +0100

    update

--- CODIGOS_FPRO/DAW/T6/Recursion/src/controller/EjerciciosNumericos.java
+++ CODIGOS_FPRO/DAW/T6/Recursion/src/controller/EjerciciosNumericos.java
@@ -11,4 +11,17 @@ public class EjerciciosNumericos {
         // return numerosFibonacci(6)+numerosFibonacci(5);
     }
 
+    // ejercicios.moviemtoHanoi(3,'A','B','C');
+    public void moviemtoHanoi(int discos, char torreOrigen, char torreDestino, char torreAuxiliar){
+        if(discos ==1){
+            System.out.println("Movimiento del disco 1 desde "+torreOrigen+" hasta "+torreDestino);
+            return;
+        }
+
+        moviemtoHanoi(discos-1, torreOrigen,torreAuxiliar,torreDestino);
+        System.out.println("Movimiento del disco "+discos+" desde "+torreOrigen+" hasta "+torreDestino);
+
+        moviemtoHanoi(discos-1, torreAuxiliar,torreDestino,torreOrigen);
+    }
+
 }
