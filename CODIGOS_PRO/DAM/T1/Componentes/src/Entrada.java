048fae4e4d3f72048171c64b8991188b9022c69e
Develop and System <developandsystem@gmail.com>
2026-09-21 16:57:01 +0200

    update

---
+++ CODIGOS_PRO/DAM/T1/Componentes/src/Entrada.java
@@ -0,0 +1,26 @@
+/**
+ * @author borja
+ * @version 1.0
+ */
+public class Entrada {
+    // aqui explicas BREVEMENTE algo para aclararte las ideas
+    // TODO esta tarea la dejo pendiente para el martes
+    // en esta otra linea hago una claracion diferente
+    /*
+    Este comentario admite unas cuantas lineas
+    Esta es la segunda linea
+     */
+    // TODO esta tarea la dejo pendiente para el lunes
+
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo");
+    }
+
+    /**
+     * @param arg explica el parametro
+     * @return explicate el retort
+     */
+    public int metodo(String arg) {
+        return 1;
+    }
+}
